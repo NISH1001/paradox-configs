2514b458951a8d09e247601f132d4ca523d6fef6
NISH1001 <nishanpantha@gmail.com>
2025-10-20 11:19:07 -0500

    some fixex and updates

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,27 +1,40 @@
 -- Source Vimscript configuration
 vim.cmd('source ~/.config/nvim/init.vim.bak')
 
--- Configure `ruff-lsp`.
--- See: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#ruff_lsp
--- For the default config, along with instructions on how to customize the settings
-require('lspconfig').ruff.setup({
-  init_options = {
-    settings = {
-      -- Any extra CLI arguments for `ruff` go here.
-      args = {},
-    }
+-- Configure `ruff-lsp` using new Neovim 0.11+ API
+-- See: :help lspconfig-nvim-0.11
+vim.lsp.config.ruff = {
+  cmd = { 'ruff-lsp' },
+  filetypes = { 'python' },
+  root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', '.git' },
+  settings = {
+    args = {},
   },
-  on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
+}
+
+-- Auto-format on save for ruff
+local augroup = vim.api.nvim_create_augroup('LspFormatting', { clear = true })
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = augroup,
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.name == 'ruff' and client.supports_method('textDocument/formatting') then
+      vim.api.nvim_create_autocmd('BufWritePre', {
         group = augroup,
-        buffer = bufnr,
+        buffer = args.buf,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr })
+          vim.lsp.buf.format({ bufnr = args.buf })
         end,
       })
     end
+  end,
+})
+
+-- Enable ruff LSP for Python files
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'python',
+  callback = function()
+    vim.lsp.enable('ruff')
   end,
 })
 
